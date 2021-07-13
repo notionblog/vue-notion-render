339147d6dbf4c8194790c50dec880c936b2fc6eb
yudax42 <yudax42@gmail.com>
2021-07-13 16:29:01 +0100

    add katex and prism js to extenrnal dependencies

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,44 +1,46 @@
 // rollup.config.js
-import fs from 'fs';
-import path from 'path';
-import vue from 'rollup-plugin-vue';
-import alias from '@rollup/plugin-alias';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import minimist from 'minimist';
+import fs from "fs";
+import path from "path";
+import vue from "rollup-plugin-vue";
+import alias from "@rollup/plugin-alias";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+import minimist from "minimist";
 
 // Get browserslist config and remove ie from es build targets
-const esbrowserslist = fs.readFileSync('./.browserslistrc')
+const esbrowserslist = fs
+  .readFileSync("./.browserslistrc")
   .toString()
-  .split('\n')
-  .filter((entry) => entry && entry.substring(0, 2) !== 'ie');
+  .split("\n")
+  .filter((entry) => entry && entry.substring(0, 2) !== "ie");
 
 // Extract babel preset-env config, to combine with esbrowserslist
-const babelPresetEnvConfig = require('../babel.config')
-  .presets.filter((entry) => entry[0] === '@babel/preset-env')[0][1];
+const babelPresetEnvConfig = require("../babel.config").presets.filter(
+  (entry) => entry[0] === "@babel/preset-env"
+)[0][1];
 
 const argv = minimist(process.argv.slice(2));
 
-const projectRoot = path.resolve(__dirname, '..');
+const projectRoot = path.resolve(__dirname, "..");
 
 const baseConfig = {
-  input: 'src/entry.js',
+  input: "src/entry.js",
   plugins: {
     preVue: [
       alias({
         entries: [
           {
-            find: '@',
-            replacement: `${path.resolve(projectRoot, 'src')}`,
+            find: "@",
+            replacement: `${path.resolve(projectRoot, "src")}`,
           },
         ],
       }),
     ],
     replace: {
-      'process.env.NODE_ENV': JSON.stringify('production'),
+      "process.env.NODE_ENV": JSON.stringify("production"),
     },
     vue: {
       css: true,
@@ -48,14 +50,14 @@ const baseConfig = {
     },
     postVue: [
       resolve({
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+        extensions: [".js", ".jsx", ".ts", ".tsx", ".vue"],
       }),
       commonjs(),
     ],
     babel: {
-      exclude: 'node_modules/**',
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
-      babelHelpers: 'bundled',
+      exclude: "node_modules/**",
+      extensions: [".js", ".jsx", ".ts", ".tsx", ".vue"],
+      babelHelpers: "bundled",
     },
   },
 };
@@ -65,7 +67,9 @@ const baseConfig = {
 const external = [
   // list external dependencies, exactly the way it is written in the import statement.
   // eg. 'jquery'
-  'vue',
+  "vue",
+  "katex",
+  "prismjs",
 ];
 
 // UMD/IIFE shared settings: output.globals
@@ -73,20 +77,20 @@ const external = [
 const globals = {
   // Provide global variable names to replace your external imports
   // eg. jquery: '$'
-  vue: 'Vue',
+  vue: "Vue",
 };
 
 // Customize configs for individual targets
 const buildFormats = [];
-if (!argv.format || argv.format === 'es') {
+if (!argv.format || argv.format === "es") {
   const esConfig = {
     ...baseConfig,
-    input: 'src/entry.esm.js',
+    input: "src/entry.esm.js",
     external,
     output: {
-      file: 'dist/vue-notion-render.esm.js',
-      format: 'esm',
-      exports: 'named',
+      file: "dist/vue-notion-render.esm.js",
+      format: "esm",
+      exports: "named",
     },
     plugins: [
       replace(baseConfig.plugins.replace),
@@ -97,7 +101,7 @@ if (!argv.format || argv.format === 'es') {
         ...baseConfig.plugins.babel,
         presets: [
           [
-            '@babel/preset-env',
+            "@babel/preset-env",
             {
               ...babelPresetEnvConfig,
               targets: esbrowserslist,
@@ -110,16 +114,16 @@ if (!argv.format || argv.format === 'es') {
   buildFormats.push(esConfig);
 }
 
-if (!argv.format || argv.format === 'cjs') {
+if (!argv.format || argv.format === "cjs") {
   const umdConfig = {
     ...baseConfig,
     external,
     output: {
       compact: true,
-      file: 'dist/vue-notion-render.ssr.js',
-      format: 'cjs',
-      name: 'VueNotionRender',
-      exports: 'auto',
+      file: "dist/vue-notion-render.ssr.js",
+      format: "cjs",
+      name: "VueNotionRender",
+      exports: "auto",
       globals,
     },
     plugins: [
@@ -139,16 +143,16 @@ if (!argv.format || argv.format === 'cjs') {
   buildFormats.push(umdConfig);
 }
 
-if (!argv.format || argv.format === 'iife') {
+if (!argv.format || argv.format === "iife") {
   const unpkgConfig = {
     ...baseConfig,
     external,
     output: {
       compact: true,
-      file: 'dist/vue-notion-render.min.js',
-      format: 'iife',
-      name: 'VueNotionRender',
-      exports: 'auto',
+      file: "dist/vue-notion-render.min.js",
+      format: "iife",
+      name: "VueNotionRender",
+      exports: "auto",
       globals,
     },
     plugins: [
